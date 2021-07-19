248bcddbaa0ac2eb53a6351b52eb48712f32b8b6
kimchavsov <kimchavsov@gmail.com>
2021-07-19 16:47:27 +0800

    Add prototype

--- app.js
+++ app.js
@@ -22,84 +22,85 @@ function Library() {
   //Library Storage
   this.myLibrary = []
 
-  // Add new book to the library
-  this.addNewBook = (title, author, page, status) => {
-    let book = new Book(title, author, page, status);
-    this.myLibrary.push(book)
-  }
-
-  // Function to shwow book in the HTML content
-  this.showBook = (item) => { 
-    // Create Element to the card
-  const div = document.createElement('div')
-  const h3 = document.createElement('h3');
-  const p1 = document.createElement('p');
-  const p2 = document.createElement('p')
-  
-  const removeBtn = document.createElement('button')
-  const readBtn = document.createElement('button')
-  
-  readBtn.classList.add('btn-read-status')
-
-  removeBtn.classList.add('btn')
-  removeBtn.classList.add('remove-btn')
-  removeBtn.innerHTML = 'Remove';
-
-  h3.textContent = `${item.title}`;
-  h3.classList.add("book-title");
-  p1.innerHTML = `Author: ${item.author}`;
-  p2.innerHTML = `Page: ${item.page}`;
-
-  this.toggleRead(!item.status, readBtn, item);
-
-  div.appendChild(h3);
-  div.appendChild(p1);
-  div.classList.add('card');
-  div.appendChild(p2);
-  div.appendChild(readBtn)
-  div.appendChild(removeBtn)
-  container.appendChild(div);
-
-  removeBtn.addEventListener('click', () => {
-    const index = this.myLibrary.indexOf(item)
-    this.myLibrary.splice(index, 1)
-    updateLocalStorage()
-    this.updateBook()
-  })
-
-  readBtn.addEventListener('click', () => {
-    this.toggleRead(readBtn.classList.contains('read'), readBtn, item);
-    updateLocalStorage()
-    this.updateBook()
-  })
 }
 
-  // Get book to show up in card
-  this.updateBook = () => {
-   this.clearBook();
-   checkLocalStorage()
-   for (let book of this.myLibrary) {
-     this.showBook(book)
-   }
- }
+// Add new book to the library
+Library.prototype.addNewBook = function(title, author, page, status) {
+  let book = new Book(title, author, page, status);
+  this.myLibrary.push(book)
+};
+
+// Function to shwow book in the HTML content
+Library.prototype.showBook = function(item) { 
+  // Create Element to the card
+const div = document.createElement('div')
+const h3 = document.createElement('h3');
+const p1 = document.createElement('p');
+const p2 = document.createElement('p')
+
+const removeBtn = document.createElement('button')
+const readBtn = document.createElement('button')
+
+readBtn.classList.add('btn-read-status')
+
+removeBtn.classList.add('btn')
+removeBtn.classList.add('remove-btn')
+removeBtn.innerHTML = 'Remove';
+
+h3.textContent = `${item.title}`;
+h3.classList.add("book-title");
+p1.innerHTML = `Author: ${item.author}`;
+p2.innerHTML = `Page: ${item.page}`;
+
+this.toggleRead(!item.status, readBtn, item);
+
+div.appendChild(h3);
+div.appendChild(p1);
+div.classList.add('card');
+div.appendChild(p2);
+div.appendChild(readBtn)
+div.appendChild(removeBtn)
+container.appendChild(div);
+
+removeBtn.addEventListener('click', () => {
+  const index = this.myLibrary.indexOf(item)
+  this.myLibrary.splice(index, 1)
+  updateLocalStorage()
+  this.updateBook()
+})
+
+readBtn.addEventListener('click', () => {
+  this.toggleRead(readBtn.classList.contains('read'), readBtn, item);
+  updateLocalStorage()
+  this.updateBook()
+})
+};
+
+// Get book to show up in card
+Library.prototype.updateBook = function() {
+  this.clearBook();
+  checkLocalStorage()
+  for (let book of this.myLibrary) {
+    this.showBook(book)
+  }
+}
  // Function to clear book from the self inorder to update the new book
- this.clearBook = () => {
+Library.prototype.clearBook = function() {
   container.innerHTML = "";
-}
-  // Set the 'Read' status
-  this.toggleRead = (condi, readBtn, item) => {
-    if (condi) {
-      readBtn.classList.remove('read');
-      readBtn.innerHTML = 'NOT READ';
-      item.status = false;
-    } else {
-      readBtn.classList.add('read');
-      readBtn.innerHTML = 'READ';
-      item.status = true
-    }
-  }
-}
+};
 
+// Set the 'Read' status
+Library.prototype.toggleRead = function(condi, readBtn, item) {
+  if (condi) {
+    readBtn.classList.remove('read');
+    readBtn.innerHTML = 'NOT READ';
+    item.status = false;
+  } else {
+    readBtn.classList.add('read');
+    readBtn.innerHTML = 'READ';
+    item.status = true
+  }
+};
 
 const library = new Library()
 // Run to check if book exist
